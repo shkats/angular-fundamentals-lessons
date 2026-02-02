ceb691656a1fa4cfa68d2e18ffabbdb926a84daf
Sharon Kats <katz.sharona@gmail.com>
2026-02-02 15:41:30 -0500

    intro

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,14 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>ETA - NewJeans</li>
+      <li>Ready Player one</li>
+      <li>Dodgeball</li>
+    </ol>
   `,
-  styles: ``,
+  styles: `ol {
+    list-style-type: upper-roman;
+}`,
 })
-export class AppComponent {}
+export class AppComponent { }
