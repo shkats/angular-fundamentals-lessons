4f62b7ecb326699fcfc36cd799bb5bd99707761e
Sharon Kats <katz.sharona@gmail.com>
2026-02-17 16:12:16 -0500

    07-08 routing

--- projects/07-routing-basics/src/app/app.component.ts
+++ projects/07-routing-basics/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <h1>Enable routing to see the greeting below</h1> `,
+  imports: [CommonModule, RouterModule],
+  template: ` <h1>Enable routing to see the greeting below</h1> 
+  <router-outlet></router-outlet>
+  `,
   styles: [],
 })
 export class AppComponent {
   title = '07-routing-basics';
+
+
 }
